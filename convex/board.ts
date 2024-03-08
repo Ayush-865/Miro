0e7847922ab745ff91a7aeab4094f1049ba6a19b
Ayush <ayush.up105@gmail.com>
2024-03-08 21:23:46 +0530

    LiveBlocks Auth

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { Id } from "./_generated/dataModel";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
 const images = [
@@ -160,6 +160,14 @@ export const unfavorite = mutation({
 
     await ctx.db.delete(existingFavorite._id);
 
+    return board;
+  },
+});
+
+export const get = query({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const board = await ctx.db.get(args.id);
     return board;
   },
 });
